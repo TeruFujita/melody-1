28b83d8c26ae08efee23ff9084f234bc2a1108fd
uma1123 <yuma1596@gmail.com>
2025-05-24 15:50:20 +0900

    HomeコンポーネントでLoginをTopに置き換え

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Login } from "@/components/login";
+import { Top } from "@/components/top";
 import { Navbar } from "@/components/navbar";
 import React from "react";
 
@@ -7,7 +7,7 @@ function Home() {
     <div className="min-h-screen bg-gradient-to-b from-pink-50 to-purple-50">
       <Navbar />
       <main className="container mx-auto px-4 py-8">
-        <Login />
+        <Top />
       </main>
     </div>
   );
